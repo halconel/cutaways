5eca1be5fe5ce6d0789f9b1770183c917278a635
Dmitry Vinogradov <di.vinogradov@gmail.com>
2019-02-09 21:14:17 +0300

    Root component in provider wraped with paper

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-/**
- * @format
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import App from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
