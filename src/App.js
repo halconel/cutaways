d24c180df21a453e4a721258d649a955a90415b0
Dmitry Vinogradov <di.vinogradov@gmail.com>
2019-02-09 22:39:31 +0300

    Refactor

--- src/App.js
+++ src/App.js
@@ -6,22 +6,9 @@ import MyAppBar from './components/MyAppBar'
 type Props = {};
 export default class App extends Component<Props> {
 
-  _goBack = () => console.log('Went back');
-  _onSearch = () => console.log('Searching');
-  _onMore = () => console.log('Shown more');
-
   render() {
     return (
         <MyAppBar />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  bottom: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-});
