1aafe85ae76013db8d7d332208a9d19a0e979d9c
Виноградов Дмитрий <di.vinogradov@gmail.com>
2019-02-10 18:09:18 +0300

    Project moved to WS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import { Appbar } from 'react-native-paper';
-import MyAppBar from './components/MyAppBar'
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { Appbar } from "react-native-paper";
+import MyAppBar from "./components/MyAppBar";
 
 type Props = {};
 export default class App extends Component<Props> {
-
   render() {
     return (
-        <MyAppBar />
+      <View style={styles.container}>
+        <Text style={{ fontWeight: "bold" }}>Hi</Text>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center"
+  }
+});
