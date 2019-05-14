e01003ca29f19d514158b8f5b7eb1dab7e5628b7
Dmitry Vinogradov <di.vinogradov@gmail.com>
2019-05-14 17:17:33 +0300

    [Debug] Fix azimuth heading (filtering)

--- src/components/RadarScreen.js
+++ src/components/RadarScreen.js
@@ -1,9 +1,9 @@
-import React, { Component, Platform } from 'react';
+import React, { Component } from 'react';
 import {
-  StyleSheet, View, Text, PermissionsAndroid, Animated, Easing,
+  StyleSheet, View, Text, PermissionsAndroid, Animated, Easing, Platform,
 } from 'react-native';
 import { IconButton, Paragraph, Title } from 'react-native-paper';
-import * as Sensors from 'react-native-sensors';
+import { setUpdateIntervalForType, magnetometer, SensorTypes } from 'react-native-sensors';
 import Arrow from './Arrow';
 import { withGlobalContext } from './GlobalContext';
 import { distance, barring, angle } from '../utils/Utils';
@@ -28,8 +28,8 @@ const styles = StyleSheet.create({
   },
 });
 
-const kf_x = new Kalman({ R: 0.01, Q: 2 });
-const kf_y = new Kalman({ R: 0.01, Q: 2 });
+const kf_x = new Kalman({ R: 1, Q: 3 });
+const kf_y = new Kalman({ R: 1, Q: 3 });
 
 const MainTitle = ({ title, voltage }) => {
   const _styles = StyleSheet.create({
@@ -62,9 +62,9 @@ const requestPermission = () => {
   return PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION).then(
     (granted) => {
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        return Promise.resolve('You can use the location');
+        return Promise.resolve(true);
       }
-      return Promise.reject('Location permission denied');
+      return Promise.reject(new Error('Location permission denied'));
     },
   );
 };
@@ -73,7 +73,11 @@ class RadarScreen extends Component {
   constructor(props) {
     super(props);
 
+    const { global: { beacons }, navigation } = this.props;
+    const id = navigation.getParam('id', '');
     this.spinValue = new Animated.Value(0);
+    this.beacon = beacons.find(b => b.key === id);
+
     this.state = {
       latitude: null,
       longitude: null,
@@ -84,16 +88,23 @@ class RadarScreen extends Component {
   }
 
   componentDidMount() {
+    const { beacon } = this;
     const posHandler = (position) => {
       const { latitude, longitude } = position.coords;
-      this.setState({ latitude, longitude });
+      const heading = latitude && barring(latitude, longitude, beacon.lat, beacon.lon);
+      this.setState({ latitude, longitude, heading });
     };
     const errHandler = error => this.setState({ error: error.message });
     const options = { enableHighAccuracy: true, timeout: 20000, distanceFilter: 10 };
-
-    navigator.geolocation.getCurrentPosition(posHandler, errHandler, options);
-    this.watchId = navigator.geolocation.watchPosition(posHandler, errHandler, options);
+    requestPermission().then((resolve) => {
+      navigator.geolocation.getCurrentPosition(posHandler, errHandler, options);
+      this.watchId = navigator.geolocation.watchPosition(posHandler, errHandler, options);
+    });
     this._toggle();
+  }
+
+  componentWillUpdate() {
+    this.spin();
   }
 
   componentWillUnmount() {
@@ -102,26 +113,21 @@ class RadarScreen extends Component {
     this._subscription = null;
   }
 
-  componentWillUpdate() {
-    this.spin();
-  }
-
   _toggle = () => {
     if (this._subscription) this._unsubscribe();
     else this._subscribe();
   };
 
   _subscribe = () => {
-    const { magnetometer } = Sensors;
-
-    this._subscription = magnetometer.subscribe( async (sensorData) => {
+    setUpdateIntervalForType(SensorTypes.magnetometer, 250);
+    this._subscription = magnetometer.subscribe(async (sensorData) => {
       const { compass } = this.state;
       let { x, y } = sensorData;
       // Kalman filter
       x = kf_x.filter(x);
       y = kf_y.filter(y);
       // Calc angle
-      const newAngle = angle({ x, y });
+      const newAngle = angle({ x, y }) - 90;
       if (compass !== newAngle) this.setState({ compass: newAngle });
     });
   };
@@ -133,34 +139,39 @@ class RadarScreen extends Component {
     let rot = +start;
     const rotM = rot % 360;
 
-    if (rotM < 180 && compass > rotM + 180) rot -= 360;
-    if (rotM >= 180 && compass <= rotM - 180) rot += 360;
+    if (rotM < 180 && (compass - heading) > rotM + 180) rot -= 360;
+    if (rotM >= 180 && (compass - heading) <= rotM - 180) rot += 360;
 
-    rot += compass - rotM;
+    rot += (compass - heading) - rotM;
 
     Animated.timing(this.spinValue, {
       toValue: rot,
       duration: 300,
-      //easing: Easing.easeInOut,
+      easing: Easing.easeInOut,
     }).start();
   }
 
   render() {
-    const {
-      global: { beacons },
-      navigation,
-    } = this.props;
-
-    const { latitude, longitude, compass } = this.state;
-    const id = navigation.getParam('id', '');
-    const beacon = beacons.find(b => b.key === id);
+    const { latitude, longitude, compass, error } = this.state;
+    const { beacon } = this;
 
     return (
       <View style={styles.container}>
         <View style={styles.green}>
           <MainTitle title={beacon.title} voltage={beacon.voltage} />
         </View>
-        <View style={styles.red} />
+        <View style={styles.red}>
+          <Text>Место положение телефона:</Text>
+          <Text>{` Широта:  ${latitude}`}</Text>
+          <Text>{` Долгота: ${longitude}`}</Text>
+          <Text>Место положение маяка:</Text>
+          <Text>{` Широта:  ${beacon.lat}`}</Text>
+          <Text>{` Долгота: ${beacon.lon}`}</Text>
+          <Text>{`Расстаяние (км.): ${latitude && distance(latitude, longitude, beacon.lat, beacon.lon)}`}</Text>
+          <Text>{`Азимут (град.): ${latitude && barring(latitude, longitude, beacon.lat, beacon.lon)}`}</Text>
+          <Text>{`Компасс (град.): ${compass}`}</Text>
+          <Text>{`Ошибка: ${JSON.stringify(error)}`}</Text>
+        </View>
         <View style={styles.yellow}>
           <Arrow spinValue={this.spinValue} />
         </View>
